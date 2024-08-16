590200f11b6141065bd908d05ad3f145786ca5e0
lobo1994 <c.b.l.lobo1994@gmail.com>
2024-08-16 10:43:01 -0500

    responsividad y validacion, v.2

--- app.js
+++ app.js
@@ -9,52 +9,66 @@ function asignarTextoElemento(elemento, texto){
 
 function encriptarPalabras(){
     let fraseUsuario = document.getElementById("entradaTexto").value; 
-    console.log(fraseUsuario);
-    let nuevaFrase=fraseUsuario.replace(/[aeiou]/g,function(match){
-        switch(match){
-            case "a":
-                return "ai";
-            case "e":
-                return "enter";
-            case "i":
-                return "imes";
-            case "o":
-                return "ober";
-            case "u":
-                return "ufat";
-        }
-            
-        });
-    let ocultar = document.getElementById("antesDeClic");
-        ocultar.style.display="none";
-        asignarTextoElemento("resultado", nuevaFrase);
-        mostrar.style.display="block";
+    const regex= /^[a-z\s]+$/;
+    if(regex.test(fraseUsuario)){
+        console.log(fraseUsuario);
+        let nuevaFrase=fraseUsuario.replace(/[aeiou]/g,function(match){
+            switch(match){
+                case "a":
+                    return "ai";
+                case "e":
+                    return "enter";
+                case "i":
+                    return "imes";
+                case "o":
+                    return "ober";
+                case "u":
+                    return "ufat";
+            }
+                
+            });
+        let ocultar = document.getElementById("antesDeClic");
+            ocultar.style.display="none";
+            asignarTextoElemento("resultado", nuevaFrase);
+            mostrar.style.display="block";
 
 
-    console.log(nuevaFrase);
+        console.log(nuevaFrase);
+    }
+    else{
+        alert("El texto solo debe contener letras minusculas"); 
+        document.getElementById("entradaTexto").value="";
+    }
 
 
 }
 function desencriptarPalabras(){
     let fraseEncriptada = document.getElementById("entradaTexto").value;
-    let nuevaFrase=fraseEncriptada.replace(/ai|enter|imes|ober|ufat/g,function(match){
-        switch(match){
-            case "ai":
-                return "a";
-            case "enter":
-                return "e";
-            case "imes":
-                return "i";
-            case "ober":
-                return "o";
-            case "ufat":
-                return "u";
+    const regex= /^[a-z\s]+$/;
+    if(regex.test(fraseEncriptada)){
+        let nuevaFrase=fraseEncriptada.replace(/ai|enter|imes|ober|ufat/g,function(match){
+            switch(match){
+                case "ai":
+                    return "a";
+                case "enter":
+                    return "e";
+                case "imes":
+                    return "i";
+                case "ober":
+                    return "o";
+                case "ufat":
+                    return "u";
 
-        }
-    });
-    asignarTextoElemento("resultado", nuevaFrase);
-    mostrar.style.display="block";
-    console.log(nuevaFrase);
+            }
+        });
+        asignarTextoElemento("resultado", nuevaFrase);
+        mostrar.style.display="block";
+        console.log(nuevaFrase);
+    }
+    else{
+        alert("El texto solo debe contener letras minusculas"); 
+        document.getElementById("entradaTexto").value="";
+    }
 }
 function copiarTexto(){
     var elementoCopiado = document.getElementById("textoCopiar");
